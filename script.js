ec6a99da4df899b00fdc4357c695facfe86366ff
Pacome-grn <pagarnier@eleve.st-charles.eu>
2025-11-25 15:03:24 +0000

    feat: Implémentation du toggle dark mode

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const titre = document.querySelector('#titre-principal');
     titre.textContent = "Titre cliqué !";
   });
 }
+// --- LOGIQUE DU DARK MODE ---
+// 1. CIBLER
+const themeButton = document.querySelector('#theme-toggle');
+
+// On vérifie si le bouton existe sur la page actuelle
+if (themeButton) {
+  // 2. ÉCOUTER
+  themeButton.addEventListener('click', () => {
+    // 3. MANIPULER
+    // On ajoute/retire la classe 'dark-mode' sur le body
+    document.body.classList.toggle('dark-mode');
+  });
+}
